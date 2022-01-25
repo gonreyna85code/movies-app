53d8b10b29a738dd5a07be8517170ee6fd9d1179
gonzalo <gonreyna85@gmail.com>
2022-01-25 18:29:37 -0300

    x

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,7 +1,7 @@
 const Router = require("express");
 
 const isAuthenticated = function (req, res, next) {
-  console.log(req.user);
+  console.log(req);
   if (req.isAuthenticated()) return next();
   res.send("No Disponible");
 };
