9e275a37b157209119b4d83d222a9779269ffec5
gonzalo <gonreyna85@gmail.com>
2022-01-25 15:34:34 -0300

    x

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,29 +1,24 @@
 const Router = require("express");
 
+const isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.send("No Disponible");
+  console.log('no logueado')
+};
+
 const router = Router();
 
-function checkAuthentication(req,res,next){
-  if(req.isAuthenticated()){
-      //req.isAuthenticated() will return true if user is logged in
-      next();
-  } else{
-      res.redirect("/login");
+router.get("/movie/:name", isAuthenticated, async (req, res) => {
+  const name = req.params.name;
+  const TorrentSearchApi = require("torrent-search-api");
+  TorrentSearchApi.enableProvider("1337x");
+  let data = await TorrentSearchApi.search(`${name}`, "Movies", 5);
+  for (let i = 0; i < data.length; i++) {
+    const magnet = await TorrentSearchApi.getMagnet(data[i]);
+    data[i].magnet = magnet;
   }
-}
-
-router.get("/movie/:name", checkAuthentication, async (req, res) => {
-  if (req.session.passport.user !== undefined) {
-    const name = req.params.name;
-    const TorrentSearchApi = require("torrent-search-api");
-    TorrentSearchApi.enableProvider("1337x");
-    let data = await TorrentSearchApi.search(`${name}`, "Movies", 5);
-    for (let i = 0; i < data.length; i++) {
-      const magnet = await TorrentSearchApi.getMagnet(data[i]);
-      data[i].magnet = magnet;
-    }
-    console.log(data);
-    res.send(data);
-  }
+  console.log(data)
+  res.send(data);
 });
 
 module.exports = router;
