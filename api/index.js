c9cd4146e3ed3a2012c7b8c378c93768fdae674c
gonzalo <gonreyna85@gmail.com>
2022-01-25 14:49:00 -0300

    x

--- api/index.js
+++ api/index.js
@@ -60,11 +60,7 @@ app.use(
       sameSite: 'none',
        secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
-        httpOnly: true,
-        domain: 'movieon.vercel.app',
-
-
-      
+        httpOnly: true,      
      }, 
   })
 );
