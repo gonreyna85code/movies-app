79b192a2af8d3d2292c43ea723a0bf52d9aaeb31
gonzalo <gonreyna85@gmail.com>
2022-01-25 14:48:17 -0300

    x

--- api/passportConfig.js
+++ api/passportConfig.js
@@ -1,7 +1,6 @@
 const User = require("./models/user");
 const bcrypt = require("bcryptjs");
 const localStrategy = require("passport-local").Strategy;
-const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
 module.exports = function (passport) {
   passport.use(
@@ -22,45 +21,16 @@ module.exports = function (passport) {
     })
   );
 
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID:
-          "660766853123-10tfek3hfs64f0t7tpvqmg0l0olhg17v.apps.googleusercontent.com",
-        clientSecret: "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",
-        callbackURL:
-          "https://api-eventy.herokuapp.com/auth/google/callback",
-      },
-      function (accessToken, refreshToken, profile, done) {
-        User.findOne({ email: profile.email }, function (err, user) {
-          if (err) {
-            return done(err);
-          }
-          if (user) {
-            return done(null, user);
-          } else {
-            const newUser = new User({
-              username: profile.displayName,
-              email: profile.email,
-              password: "",
-            });
-            newUser.save(function (err) {
-              if (err) {
-                throw err;
-              }
-              return done(null, newUser);
-            });
-          }
-        });
-      }
-    )
-  );
+  
 
   passport.serializeUser((user, done) => {
+    console.log(user);
     done(null, user);
+    
   });
 
   passport.deserializeUser((user, done) => {
+    console.log(user);
     done(null, user);
   });
 };
