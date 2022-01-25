b67ee50bc0b064281728b73d836683e9989a072c
gonzalo <gonreyna85@gmail.com>
2022-01-25 03:33:47 -0300

    model upgraded

--- api/models/user.js
+++ api/models/user.js
@@ -7,14 +7,7 @@ const user =  mongoose.Schema({
   password: String,
   profile: Object,
   name: String,
-  email: String,
-  Acceskey: String,   //esto es Public Key 
-  publicKey: String,  //esto es Acces Token
-  events: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
-  follows: [{type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  near: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
-  subscriptions: [{ type: mongoose.Schema.Types.String, ref: "Event" }],
-  promises: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+  email: String,  
 });
 
 module.exports = mongoose.model("User", user);
