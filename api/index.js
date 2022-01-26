c2f75e4b53312f5c84ca57bd8f68a805d46a951f
gonzalo <gonreyna85@gmail.com>
2022-01-25 22:55:49 -0300

    x

--- api/index.js
+++ api/index.js
@@ -26,13 +26,7 @@ app.set("trust proxy", 1);
 
 app.all('*',function (req,res,next)
 {
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization ,Accept');
-  res.setHeader('Access-Control-Allow-Origin', 'https://movieon.vercel.app');
-  res.setHeader('Access-Control-Allow-Credentials',true);
-  res.setHeader('Access-Control-Expose-Headers','Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-  res.writeHead(200, headers);
+  
   if (req.method === "OPTIONS") {
     console.log("OPTIONS SUCCESS");
     res.next();
