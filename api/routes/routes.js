f60492ebbeb583048333690590986a9634fe90d9
gonzalo <gonreyna85@gmail.com>
2022-01-26 15:41:51 -0300

    x

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -10,21 +10,20 @@ const router = Router();
 
 router.get("/movie/:name", async (req, res) => {
   const name = req.params.name;
+  console.log(name);
   const TorrentSearchApi = require("torrent-search-api");
   try {
-  TorrentSearchApi.enablePublicProviders();
-  const data = await TorrentSearchApi.search(`${name}`, "Movies", 5);
-  for (let i = 0; i < data.length; i++) {
-    const magnet = await TorrentSearchApi.getMagnet(data[i]);
-    data[i].magnet = magnet;
+    TorrentSearchApi.enablePublicProviders();
+    const data = await TorrentSearchApi.search(`${name}`, "Movies", 5);
+    for (let i = 0; i < data.length; i++) {
+      const magnet = await TorrentSearchApi.getMagnet(data[i]);
+      data[i].magnet = magnet;
+    }
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    res.send("No Disponible");
   }
-  res.send(data);
-} catch (error) {
-  console.log(error);
-  res.send('No Disponible');
-} 
-  
 });
-
 
 module.exports = router;
