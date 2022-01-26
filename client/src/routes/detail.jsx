8353c7daab77d815f11d1c906c54d164983e13c7
gonzalo <gonreyna85@gmail.com>
2022-01-26 16:24:44 -0300

    x

--- client/src/routes/detail.jsx
+++ client/src/routes/detail.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export default function Detail(props) {
   const id = props.match.params.id;
   console.log(id);
-  const mo = useSelector((state) => state.Movie);
+  
   
   const image = 'https://image.tmdb.org/t/p/w500'+ movie?.poster_path;
   const dispatch = useDispatch();
@@ -16,9 +16,10 @@ export default function Detail(props) {
   console.log(movie)
 
     const handleHome = () => {
-      setMovie(null);
+      setMovie();
     };
-    const [movie, setMovie] = useState(mo);
+    const pelicula = useSelector((state) => state.Movie);
+    const [movie, setMovie] = useState(pelicula);
   return (
     <div>
       <div className="detail">
