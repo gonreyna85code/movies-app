c3d306f62d25105cb721e5e8c8604588205e7ec7
gonzalo <gonreyna85@gmail.com>
2022-01-26 15:08:42 -0300

    x

--- api/index.js
+++ api/index.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     res.next()
   }
   res.header('Access-Control-Allow-Origin', 'https://movieon.vercel.app')
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
  
