799ff3f59aa55b05a300de83ce51cdf6ee129ddb
gonzalo <gonreyna85@gmail.com>
2022-01-25 23:47:36 -0300

    X

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(
 app.set('trust proxy', true);
 
 app.use((req, res, next) => {
-  console.log(req.headers.cookie)
+  console.log('COOKIE:',req.headers.cookie)
   if(req.method === 'OPTIONS') {
     res.next()
   }
