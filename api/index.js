45dfabd981f54bda7aa827d46c4514b6cb1415af
gonzalo <gonreyna85@gmail.com>
2022-01-25 22:43:14 -0300

    x

--- api/index.js
+++ api/index.js
@@ -24,19 +24,19 @@ app.use(
 
 app.set("trust proxy", 1);
 
-app.use("/", (req, res, next) => {
-  console.log(req.headers);
-  headers["Access-Control-Allow-Origin"] = ["https://movieon.vercel.app"];
-  headers["Access-Control-Allow-Headers"] =
-    ["Content-Type, Content-Length, Authorization, Accept, X-Requested-With"];
-  headers["Access-Contrl-Allow-Methods"] = ["PUT, POST, GET, DELETE, OPTIONS"];
-  headers["Access-Control-Max-Age"] = ["86400"];
-  res.writeHead(200, headers);
-  if (req.method === "OPTIONS") {
-    console.log("OPTIONS SUCCESS");
-    res.end();
-  }
-});
+// app.use("/", (req, res, next) => {
+//   console.log(req.headers);
+//   headers["Access-Control-Allow-Origin"] = ["https://movieon.vercel.app"];
+//   headers["Access-Control-Allow-Headers"] =
+//     ["Content-Type, Content-Length, Authorization, Accept, X-Requested-With"];
+//   headers["Access-Contrl-Allow-Methods"] = ["PUT, POST, GET, DELETE, OPTIONS"];
+//   headers["Access-Control-Max-Age"] = ["86400"];
+//   res.writeHead(200, headers);
+//   if (req.method === "OPTIONS") {
+//     console.log("OPTIONS SUCCESS");
+//     res.end();
+//   }
+// });
 
 mongoose.connect(process.env.MONGO, () => {
   console.log("Mongoose Is Connected");
