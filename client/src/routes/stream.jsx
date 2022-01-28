01722a820d10057cafcc36a18bc85ab324620c3a
gonzalo <gonreyna85@gmail.com>
2022-01-28 12:42:56 -0300

    x

--- client/src/routes/stream.jsx
+++ client/src/routes/stream.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Stream() {
+    const magnet = require.params
     return <video id="videoPlayer" width="650" controls muted="muted" autoplay>
     <source
-      src="https://movion-back.herokuapp.com/video"
+      src={`https://movion-back.herokuapp.com/video/${magnet}`}
       type="video/mp4"
     />
   </video>;
