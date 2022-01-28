2da2c64cd06a79f461f57570307eee683617d7eb
gonzalo <gonreyna85@gmail.com>
2022-01-27 21:10:51 -0300

    x

--- client/src/routes/detail.jsx
+++ client/src/routes/detail.jsx
@@ -92,7 +92,7 @@ export default function Detail(props) {
                 torrents?.map((e) => (
                   <li>
                     {e.title} <br />
-                    <button onClick={(e) => handleTorrent(e.target.value)} value={e}>Magnet Link</button>
+                    <button onClick={(e) => handleTorrent(e.target.value)} value={e.desc}>Magnet Link</button>
                     {e.size}
                   </li>
                 ))
