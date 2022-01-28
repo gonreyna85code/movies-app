c43a8d8b5137ab133aebe784b19fe0de68c2f0b2
gonzalo <gonreyna85@gmail.com>
2022-01-27 23:24:43 -0300

    x

--- client/src/routes/detail.jsx
+++ client/src/routes/detail.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import WebTorrent from "webtorrent";
 import {
   getMovie,
   getTorrents,
@@ -10,7 +9,7 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "../styles/detail.css";
-const parseTorrent = require("parse-torrent");
+import torrentStream from "torrent-stream";
 
 export default function Detail(props) {
   const id = props.match.params.id;
@@ -38,29 +37,22 @@ export default function Detail(props) {
 
   
 
-  var link = parseTorrent(
-    "magnet:?xt=urn:btih:0D1279FC43D98EEC98329E7846D02C834962A21F&dn=Peacemaker.2022.S01E04.720p.WEB.x265-MiNX%5BTGx%5D&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.birkenwald.de%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentor.org%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2970%2Fannounce&tr=https%3A%2F%2Ftracker.foreverpirates.co%3A443%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce"
-  );
-  var link2 = parseTorrent.toTorrentFile(link);
-  
-  console.log(link2)
-  const handleTorrent = () => {
-    
-    var client = new WebTorrent();
-    client.add(link2, function (torrent) {
-      console.log(torrent); 
-      var file = torrent.files.find(function (file) {
-        return file.name.endsWith(".mp4", ".mkv");
-      });      
-      file.appendTo("body");
-    });
-  };
+  var engine = torrentStream("magnet:?xt=urn:btih:DC2ED58F9F4F6A838B395F9A27EEB30BB6CE7EDB&dn=The.Book.of.Boba.Fett.S01E05.720p.WEBRip.x265-MiNX%5BTGx%5D&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.birkenwald.de%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentor.org%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2970%2Fannounce&tr=https%3A%2F%2Ftracker.foreverpirates.co%3A443%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce");
 
+  engine.on("ready", function () {
+    engine.files.forEach(function (file) {
+      console.log("filename:", file.name);
+      var stream = file.createReadStream();
+      // stream is readable stream to containing the file content
+      stream.appendTo('body')
+    });
+  });
+
+  const handleTorrent = () => {};
 
   while (Number(id) !== Number(movie?.id)) {
     return <div className="loading">Loading...</div>;
   }
-
   return (
     <div>
       <div className="detail">
@@ -100,10 +92,7 @@ export default function Detail(props) {
                 torrents?.map((e) => (
                   <li>
                     {e.title} <br />
-                    <button
-                      onClick={handleTorrent}
-                      value={e.magnet}
-                    >
+                    <button onClick={handleTorrent} value={e.magnet}>
                       Magnet Link
                     </button>
                     {e.size}
