7a020aa0830511421cf8d0603fb273b6d0486831
gonzalo <gonreyna85@gmail.com>
2022-01-27 22:07:47 -0300

    x

--- client/src/routes/detail.jsx
+++ client/src/routes/detail.jsx
@@ -41,12 +41,14 @@ export default function Detail(props) {
   var link = parseTorrent(
     "magnet:?xt=urn:btih:0D1279FC43D98EEC98329E7846D02C834962A21F&dn=Peacemaker.2022.S01E04.720p.WEB.x265-MiNX%5BTGx%5D&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.birkenwald.de%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentor.org%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2970%2Fannounce&tr=https%3A%2F%2Ftracker.foreverpirates.co%3A443%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce"
   );
-
+  var link2 = parseTorrent.toTorrentFile(link);
+  
+  console.log(link2)
   const handleTorrent = () => {
-    var link2 = "0d1279fc43d98eec98329e7846d02c834962a21f";
+    
     var client = new WebTorrent();
     client.add(link2, function (torrent) {
-      console.log(torrent.files); 
+      console.log(torrent); 
       var file = torrent.files.find(function (file) {
         return file.name.endsWith(".mp4", ".mkv");
       });      
@@ -58,7 +60,7 @@ export default function Detail(props) {
   while (Number(id) !== Number(movie?.id)) {
     return <div className="loading">Loading...</div>;
   }
-  
+
   return (
     <div>
       <div className="detail">
