dc1e75e7f77149c0cda56d5a44e9e0e5379c75cf
gonzalo <gonreyna85@gmail.com>
2022-01-28 12:21:40 -0300

    x

--- client/src/routes/detail.jsx
+++ client/src/routes/detail.jsx
@@ -93,6 +93,9 @@ export default function Detail(props) {
           </div>
         </div>
       </div>
+      <video id="videoPlayer" width="650" controls muted="muted" autoplay>
+      <source src="https://movion-back.herokuapp.com/video" type="video/mp4" />
+    </video>
     </div>
   );
 }
