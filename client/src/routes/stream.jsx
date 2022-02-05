4e902d5aad9b0eade33954fbea8873adcbfad00a
gonzalo <gonreyna85@gmail.com>
2022-02-05 17:29:11 -0300

    x

--- client/src/routes/stream.jsx
+++ client/src/routes/stream.jsx
@@ -5,11 +5,14 @@ export default function Stream(params) {
   const magnet =
     params.match.params.magnet.toString(params.location.search) +
     params.location.search.toString();
+    const buffer = `https://movion-back.herokuapp.com/video/${magnet}`
+    console.log(buffer.length)
+
 
   return (
     <div className="streamer">
     <video id="videoPlayer" width="650" controls muted="muted" autoPlay>
-      <source src={`https://movion-back.herokuapp.com/video/${magnet}`} type="video/mp4" />
+      <source src={buffer} type="video/mp4" />
     </video>
     </div>
   );
