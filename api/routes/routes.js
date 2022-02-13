b792c8f6e032c3c438d7f3631e40436d70726e1a
gonzalo <gonreyna85@gmail.com>
2022-02-13 03:53:40 -0300

    x

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -119,7 +119,7 @@ router.get("/subtitulo/:id", async (req, res) => {
       url: file.link,
     });
     const data = subtitulo.data;   
-    data.pipe(srt2vtt()).pipe(fs.createWriteStream(res))      
+    data.srt2vtt().pipe(fs.createWriteStream(res))      
   } catch (error) {
     console.log(error);
   }
