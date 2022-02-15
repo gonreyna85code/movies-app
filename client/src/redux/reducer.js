b7eef0aba94208c361c121401f69dfdc04b4959f
gonzalo <gonreyna85@gmail.com>
2022-02-15 15:26:46 -0300

    x

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,4 +1,12 @@
-import { GET_MOVIES, GET_MOVIE, GET_SEARCH, GET_USER, GET_TORRENTS, DEL_TORRENT, GET_SUBS } from "./actions";
+import {
+  GET_MOVIES,
+  GET_MOVIE,
+  GET_SEARCH,
+  GET_USER,
+  GET_TORRENTS,
+  DEL_TORRENT,
+  GET_SUBS,
+} from "./actions";
 
 const initialState = {
   Movies: [],
@@ -18,16 +26,16 @@ function rootReducer(state = initialState, action) {
   }
   if (action.type === GET_MOVIE) {
     return {
-      ...state, 
+      ...state,
       Movie: action.payload,
     };
   }
   if (action.type === GET_SUBS) {
     return {
-      ...state, 
+      ...state,
       Subs: action.payload,
     };
-  }  
+  }
   if (action.type === DEL_TORRENT) {
     return {
       ...state,
@@ -35,22 +43,40 @@ function rootReducer(state = initialState, action) {
     };
   }
   if (action.type === GET_TORRENTS) {
-    const torrents = action.payload
-      for(let i = 0; i < action.payload.length; i++) {      
-      if(typeof torrents[i].size === 'string' && torrents[i].size.endsWith("GB") ){
-      torrents[i].size = torrents[i].size.slice(0, -3);
-      torrents[i].size  = Number(torrents[i].size)*1000}
-      if(typeof torrents[i].size === 'string' && torrents[i].size.endsWith("MB")){
-      torrents[i].size = torrents[i].size.slice(0, -3)
-      torrents[i].size  = Number(torrents[i].size)}     
-    } 
-    torrents.sort((a, b) => {
-      return a.size - b.size;
-    });
-    
+    const torrent = () => {
+        if (
+        action.payload === "Not Found" ||
+        action.payload === "No Disponible"
+      ) {
+        return action.payload;
+      } else {
+        
+         for (let i = 0; i < action.payload.length; i++) {
+          if (
+            typeof action.payload[i].size === "string" &&
+            action.payload[i].size.endsWith("GB")
+          ) {
+            action.payload[i].size = action.payload[i].size.slice(0, -3);
+            action.payload[i].size = Number(action.payload[i].size) * 1000;
+          }
+          if (
+            typeof action.payload[i].size === "string" &&
+            action.payload[i].size.endsWith("MB")
+          ) {
+            action.payload[i].size = action.payload[i].size.slice(0, -3);
+            action.payload[i].size = Number(action.payload[i].size);
+          }
+        }
+        action.payload.sort((a, b) => {
+          return a.size - b.size;
+        });
+        return action.payload;
+      }
+    };
+    console.log(torrent())
     return {
       ...state,
-      Torrents: action.payload,
+      Torrents: torrent(),
     };
   }
   if (action.type === GET_USER) {
